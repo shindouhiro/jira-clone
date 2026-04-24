06260dc056641bfff34c07c95e085dba8b55d531
Hikaru <shindouhiro@gmail.com>
2026-04-24 13:50:22 +0800

    feat(eslint): 启用unocss支持
    - 将eslint配置中的unocss选项从false改为true
    - 以便在项目中启用unocss样式检查和格式化功能

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import antfu from '@antfu/eslint-config'
 export default antfu(
   {
     formatters: true,
-    unocss: false,
+    unocss: true,
     vue: true,
     typescript: true,
     yaml: true,
